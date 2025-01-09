7b984a0b60f58a685b7ffb1dbf6fe1281c1d93be
ceosofts <ceosofts@gmail.com>
2025-01-09 19:08:20 +0700

    9 jan 2025 ยังไม่ได้

--- CeosoftsPy/backend/app.py
+++ CeosoftsPy/backend/app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -19,3 +20,5 @@ from app import routes
 
 if __name__ == "__main__":
     app.run(debug=True)
+=======
+>>>>>>> parent of 25da8d4e (login system finish)
